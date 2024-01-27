64dd214dca4e0cfcc4a9d8fd830638de7e4403ad
nanasir0 <69567419+nanasir0@users.noreply.github.com>
2024-01-27 19:37:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'node_modules/three';
+import * as THREE from '/three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
