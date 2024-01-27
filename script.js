f7f04d882ba7a955257da9b76b3aa8817e9f7a84
nanasir0 <69567419+nanasir0@users.noreply.github.com>
2024-01-27 19:55:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as THREE from '/three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '/three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls.js';
 
 // for shooting star
 import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline';
